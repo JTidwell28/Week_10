1b87c981a27fe5165f39ffcf06aeacdf1deb6ac4
Tidwell <jontidwell@FVFZMY9SJ1WK.tld>
2022-03-25 14:49:36 -0500

    Final Commit

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
-function nameWhat(){
+function nameWhat() {
     var name = prompt("What's your name?")
     alert(`Hello ${name}!`)
     return name
 }
-
-
-
-
-function convert(){
+function convert() {
     var pick = prompt(`Would you like to convert Fº to Cº type "F". If you would like to convert Cº to Fº type "C".`)
     return pick
 }
-
-function faren(pick){
-    if (convert() == "F"){
-        var tempo1 = Number(prompt("What is the tempature out side in FARENHEIGHT?!"))
-        tempo2 = (tempo1 - 32) * 5 /9
-        var h = alert(`Your temp is F is ${pick} and in C it is ${tempo2}`) 
-        return h
-    }
+function numberout() {
+    var numout = Number(prompt("What is the tempature outside?"))
+    return numout
 }
-
-nameWhat()  , faren()
+function faren(numout) {
+    
+    tempo2 = (numout - 32) * 5 / 9 
+    var h = alert(`Your temp in F is ${numout} and in C it is ${tempo2}`) 
+    return h
+}
+function celc(numout) {    
+    celc2 = numout  * 9 / 5 + 32
+    var c = alert(`Your temp in F is ${numout} and in celcius its ${celc2}`)
+    return c    
+}
+nameWhat() 
+if (convert() == "F") {
+    faren(numberout())
+}else {
+    celc(numberout())
+}
